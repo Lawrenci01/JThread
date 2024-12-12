7ab8c4804e75f374571143095684ed58189f6e90
Lawrenci01 <rlnodado@gbox.ncf.edu.ph>
2024-12-12 12:50:09 +0800

    Update connectDB.js

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,18 +1,20 @@
-import mongoose from "mongoose";
+import dotenv from 'dotenv';  
+import mongoose from 'mongoose';  
 
-const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI, {
-			// To avoid warnings in the console
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
+dotenv.config(); // Load environment variables  
 
-		console.log(`MongoDB Connected: ${conn.connection.host}`);
-	} catch (error) {
-		console.error(`Error: ${error.message}`);
-		process.exit(1);
-	}
-};
+const connectDB = async () => {  
+	try {  
+		console.log(`Connecting to MongoDB using URI: ${process.env.MONGO_URI}`); // Debugging line  
+		const conn = await mongoose.connect(process.env.MONGO_URI, {  
+			useNewUrlParser: true,  
+			useUnifiedTopology: true,  
+		});  
+		console.log(`MongoDB Connected: ${conn.connection.host}`);  
+	} catch (error) {  
+		console.error(`Error: ${error.message}`);  
+		process.exit(1);  
+	}  
+};  
 
 export default connectDB;
